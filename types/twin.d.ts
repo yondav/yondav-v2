7e648f850bade922dcf30d385632c7083e7d2bf6
yondav <yoni@yondav.us>
2024-03-06 22:43:07 -0500

    chore: still addressing build issue

--- types/twin.d.ts
+++ types/twin.d.ts
@@ -1,40 +1,5 @@
-// import type { CSSProp, css as cssImport } from 'styled-components';
-// import type styledImport from 'styled-components';
-// import 'twin.macro';
-
-// declare module 'twin.macro' {
-//   // The styled and css imports
-//   const styled: typeof styledImport;
-//   const css: typeof cssImport;
-// }
-
-// declare module 'react' {
-//   // The css prop
-//   interface HTMLAttributes<T> extends DOMAttributes<T> {
-//     css?: CSSProp;
-//     tw?: string;
-//   }
-//   // The inline svg css prop
-//   interface SVGProps<T> extends SVGProps<SVGSVGElement> {
-//     css?: CSSProp;
-//     tw?: string;
-//   }
-// }
-
-// // The 'as' prop on styled components
-// declare global {
-//   namespace JSX {
-//     interface IntrinsicAttributes<T> extends DOMAttributes<T> {
-//       as?: string | Element;
-//     }
-//   }
-// }
-
-import type { DOMAttributes } from 'framer-motion';
-import { motion } from 'framer-motion';
-import type { JSXElementConstructor } from 'react';
 import type { CSSProp, css as cssImport } from 'styled-components';
-import styledImport from 'styled-components';
+import type styledImport from 'styled-components';
 import 'twin.macro';
 
 declare module 'twin.macro' {
@@ -49,18 +14,11 @@ declare module 'react' {
     css?: CSSProp;
     tw?: string;
   }
-}
-
-// Create a custom function that wraps the styled function and handles motion components
-function styledWithMotion<E extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>>(
-  element: E
-) {
-  return function (
-    ...args: Parameters<typeof styledImport> // Pass through the parameters of the original styled function
-  ) {
-    const StyledComponent = styledImport(element)(...args); // Call the original styled function with the specified element
-    return motion(StyledComponent); // Wrap the styled component with framer-motion's motion HOC
-  };
+  // The inline svg css prop
+  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+    css?: CSSProp;
+    tw?: string;
+  }
 }
 
 // The 'as' prop on styled components
