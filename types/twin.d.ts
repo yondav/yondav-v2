8a98ff2892ca7228b3c1e4eb88edefde1eba3719
yondav <yoni@yondav.us>
2024-03-06 22:38:06 -0500

    chore: still assessing build issue

--- types/twin.d.ts
+++ types/twin.d.ts
@@ -1,4 +1,38 @@
-import type { DOMAttributes } from 'react';
+// import type { CSSProp, css as cssImport } from 'styled-components';
+// import type styledImport from 'styled-components';
+// import 'twin.macro';
+
+// declare module 'twin.macro' {
+//   // The styled and css imports
+//   const styled: typeof styledImport;
+//   const css: typeof cssImport;
+// }
+
+// declare module 'react' {
+//   // The css prop
+//   interface HTMLAttributes<T> extends DOMAttributes<T> {
+//     css?: CSSProp;
+//     tw?: string;
+//   }
+//   // The inline svg css prop
+//   interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+//     css?: CSSProp;
+//     tw?: string;
+//   }
+// }
+
+// // The 'as' prop on styled components
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+//       as?: string | Element;
+//     }
+//   }
+// }
+
+import type { DOMAttributes } from 'framer-motion';
+import { motion } from 'framer-motion';
+import type { JSXElementConstructor } from 'react';
 import type { CSSProp, css as cssImport } from 'styled-components';
 import styledImport from 'styled-components';
 import 'twin.macro';
@@ -17,15 +51,15 @@ declare module 'react' {
   }
 }
 
-// Create a custom function that wraps the styled function and handles motion components with any HTML element type
+// Create a custom function that wraps the styled function and handles motion components
 function styledWithMotion<E extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>>(
   element: E
 ) {
   return function (
     ...args: Parameters<typeof styledImport> // Pass through the parameters of the original styled function
   ) {
-    const styledComponent = styledImport(...args); // Call the original styled function
-    return styledComponent as any; // Cast the styled component to any to avoid type errors
+    const StyledComponent = styledImport(element)(...args); // Call the original styled function with the specified element
+    return motion(StyledComponent); // Wrap the styled component with framer-motion's motion HOC
   };
 }
 
